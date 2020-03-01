c13d210d753b074e56f541605be22f34b223011a
moai891 <ktdkmk2@gmail.com>
2020-03-01 20:03:24 +0900

    edit deploy.rb
    config/deploy.rbにローカルPCのEC2インスタンスのSSH鍵へのパスを入力

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス（例：~/.ssh/key_pem.pem）>'] 
+                  keys: ['~/.ssh/freemarket_sample_64c.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
