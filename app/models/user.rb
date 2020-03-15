3ee201eb8c8d5d033b175c63f70d9d60d4df3194
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-03-15 16:14:55 +0900

    create user model
    1. Userモデル作成
    ＄rails g devise User
    
    2. マイグレーションファイル編集
    GSSの内容をもとに編集
    
    3. マイグレーション実行
    $rails db:migrate
    
    4. models/user.rb編集
    GSSの内容をもとに編集

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_one :card, dependent: :destroy
+  has_one :addresses, dependent: :destroy
+  validates :nickname, presence: true
+  validates :familyname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみで入力してください" }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみで入力してください" }
+  validates :familyname_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: "全角カタカナのみで入力してください" }
+  validates :firstname_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: "全角カタカナのみで入力してください" }
+end
