6e231a50174980b73bfd642690888aa545706b4a
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-03-19 13:57:15 +0900

    パスワードの最少文字数を7文字に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 7..128
 
   has_many :items, dependent: :destroy
   has_many :likes, dependent: :destroy
