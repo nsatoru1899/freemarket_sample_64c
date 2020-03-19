9072ba2b22118abb820c45753ee725460b6c5d7b
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-03-19 12:49:47 +0900

    モデル、ビューの更新

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
+  validates :postal_code, presence: true, numericality: true
+  validates :prefecture_code, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 48 }
+  validates :city, presence: true
+  validates :block, presence: true
+  
 end
