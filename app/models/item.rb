02b0ce795cbf1f56868409f834eb31ab2aecc4ba
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-03-28 11:46:23 +0900

    バリデーションの記法を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,8 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   accepts_nested_attributes_for :brand
   accepts_nested_attributes_for :images, allow_destroy: true
-  validates :detail, presence: true
+  validates :detail, :status_id, :charge_id, :prefecture_code, :day_id, :category_id, presence: true
   validates :price, presence: true, numericality: true
-  validates :status_id, presence: true
-  validates :charge_id, presence: true
-  validates :prefecture_code, presence: true
-  validates :day_id, presence: true
-  validates :category_id, presence: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :charge
