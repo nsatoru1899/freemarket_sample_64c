30ad53e5b18e31ca8dc0f6ec80a54da97ca9f083
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-04-05 12:55:42 +0900

    各種ファイルアップデート
    - applocation.scss
    - routes.rb
    - Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
   root 'tests#index'
   resources :tests
+  resources :items, only: [:new, :create]
 
   devise_scope :user do
     post 'users/sign_up/complete' => 'users/registrations#complete'
