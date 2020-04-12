a9a8886bf7c6af8fe0761bc399cfadd407a4a2e9
moai891 <ktdkmk2@gmail.com>
2020-04-12 22:08:09 +0900

    edit rspec test for cards_controller.rb
    rspecを用いたcards_controller.rbのテスト

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    user
+    card_id              { "car_324c08f76c6e07b71870d6734d78" }
+    customer_id          { "cus_e3c924dfdd64e565b3ced3f5a959" }
   end
 end
