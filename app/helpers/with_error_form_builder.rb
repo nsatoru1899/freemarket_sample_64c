c6eba69a51fa9a23a3020754cc4661afa374c21c
shogo-prog <59284171+shogo-prog@users.noreply.github.com>
2020-04-14 17:46:31 +0900

    update-sider-error(fin)

--- app/helpers/with_error_form_builder.rb
+++ app/helpers/with_error_form_builder.rb
@@ -2,7 +2,7 @@ class WithErrorFormBuilder < ActionView::Helpers::FormBuilder
   def pick_errors(attribute)
     return nil if @object.nil? || (messages = @object.errors.messages[attribute]).nil?
     lis = messages.collect do |message|
-      %{<li class="error-message">#{@object.errors.full_message(attribute, message)}</li>}
+    %{<li class="error-message">#{@object.errors.full_message(attribute, message)}</li>}
   end.join
 
       %{<ul class="error">#{lis}</ul>}.html_safe
@@ -32,7 +32,7 @@ class WithErrorFormBuilder < ActionView::Helpers::FormBuilder
     return super if options[:no_errors]
     super + pick_errors(attribute)
   end   
-     
+
   def collection_select(attribute, select, key, value, options={})
     return super if options[:no_errors]
     super + pick_errors(attribute)
