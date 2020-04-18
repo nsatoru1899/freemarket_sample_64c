b8c660fabc9d5072c19c2a3214546373be929398
shogo-prog <59284171+shogo-prog@users.noreply.github.com>
2020-04-18 15:17:16 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -76,7 +76,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     resource.update_without_current_password(params)
-    
   end
 
 end
