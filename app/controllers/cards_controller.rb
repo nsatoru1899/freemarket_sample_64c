4656db72e8447a37fe9c2b41fd4f2a3f31258fdd
moai891 <ktdkmk2@gmail.com>
2020-04-19 22:25:55 +0900

    create_card_function
    ・クレジットカード情報の表示及び削除機能の実装
    ・cards_controller.rbについてリファクタリング（set_card）

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,21 +1,19 @@
 class CardsController < ApplicationController
-  # Payjpの利用、APIキーの設定
-  require "payjp"
-  Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+# Payjpの利用、APIキーの設定
+require "payjp"
+before_action :set_card, except: [:create]
 
   # カードを既に登録していたらトップページに遷移
   def new
-    card = Card.where(user_id: current_user.id)
-    if card.exists?
-      redirect_to root_path
-    else
+    if @card.blank?
       @card = Card.new
+    else
+      redirect_to root_path
     end
   end
 
   def create
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
@@ -35,18 +33,19 @@ class CardsController < ApplicationController
   end
 
   def show
-    card = Card.where(user_id: current_user.id).first
-    @card = card
-    if card.blank?
+    # @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
       render :show
     else
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(card.customer_id)
+    customer = Payjp::Customer.retrieve(@card.customer_id)
     # Payjpのcustomerオブジェクトのカードオブジェクトを抽出する
-    @card = customer.cards.retrieve(card.card_id)
+    @card = customer.cards.retrieve(@card.card_id)
     # 登録しているカード会社のブランドアイコンを表示する
     @card_brand = @card.brand      
+
     case @card_brand
+
     when "Visa"
       @card_src = "visa.svg"
     when "JCB"
@@ -66,18 +65,27 @@ class CardsController < ApplicationController
   end
 
   def destroy #PayjpのサーバーとDBの情報の削除
-    #削除したいカードの情報を持ってくる。current_userのカード情報
-    card = Card.where(user_id: current_user.id)
-    if card.blank?
-      render :new
+
+    if @card.blank?
+      render :show
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      # Payjoのcustomerオブジェクトを取得。引数にPayjpのcutomer_idトークンを持たせる
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      card =customer.retrieve(card.card_id)
+      # Payjpのcustomerオブジェクトを取得。引数にPayjpのcutomer_idトークンを持たせる
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+    end
+
+    if @card.destroy
       customer.delete
-      card.delete
-      redirect_to_root_path
+      redirect_to action: "show"
+    else
+      redirect_to action: "show"
     end
   end
+
+  private
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first
+  end
+
 end
