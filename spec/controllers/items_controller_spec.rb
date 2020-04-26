154e8b93215191985950f41f223764f234798474
taruyusung <58249888+taruyusung@users.noreply.github.com>
2020-04-26 12:29:34 +0900

    create_controller_file
    出品機能の作成及び修正( 2 )

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,36 @@
+ require "rails_helper"
+
+RSpec.describe ItemsController, type: :controller do
+
+  let(:user) {create(:user)}
+  
+  describe "GET #new" do
+    it "ログインしていないとログインページに遷移する" do
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+    end
+    it "new.html.hamlに遷移すること" do
+      login user
+      get :new
+      expect(response).to render_template :new
+   end
+  end
+
+end
+ 
+  
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+ 
