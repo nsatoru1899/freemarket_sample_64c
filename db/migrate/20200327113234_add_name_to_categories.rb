55127234deede750c8026b9d6a2a17b3225bbe93
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-04-29 19:28:56 +0900

    categoriesのマイグレーションファイル修正

--- db/migrate/20200327113234_add_name_to_categories.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToCategories < ActiveRecord::Migration[5.0]
-  def change
-    add_column :categories, :name, :string, null:false
-    add_column :categories, :ancestry, :string, null:false
-    add_index :categories, :ancestry
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200327113234) do
+ActiveRecord::Schema.define(version: 20200326152509) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",         null: false
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 20200327113234) do
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string   "name",       null: false
     t.string   "ancestry",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
