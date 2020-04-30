0a797e4f21370f1b0909433ac7af54e2fa6b76ab
nsatoru1899 <57213208+nsatoru1899@users.noreply.github.com>
2020-04-30 21:44:42 +0900

    Siderエラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(items_params)
     @item.brand.destroy if @item.brand.name == ""
     if @item.save
