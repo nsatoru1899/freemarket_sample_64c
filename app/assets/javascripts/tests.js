749cb87aa61f9c4f199174bf34db6ed470d59099
shogo-prog <59284171+shogo-prog@users.noreply.github.com>
2020-05-01 01:10:38 +0900

    create-dropdown-category(1)
    #What
    カテゴリーのドロップダウンでの実装
    #Why
    機能の向上

---
+++ app/assets/javascripts/tests.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".header__contents__nav--left--category").hover(function() {
+  $("ul.category1").toggle();
+  });
+  $(".header__contents__nav--left--category li ul").hide();
+  $(".header__contents__nav--left--category li").hover(function() {
+	$(">ul:not(:animated)", this).stop(true, true).slideDown("fast");
+	$("child.name", this).addClass("active");
+  }, function() {
+	$(">ul:not(:animated)", this).stop(true, true).slideUp("fast");
+	$("grandchild.name", this).removeClass("active");
+	  });
+});
