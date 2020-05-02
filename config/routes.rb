965bca8494c3c4337543df7f2a0825ac84992663
shogo-prog <59284171+shogo-prog@users.noreply.github.com>
2020-05-02 09:46:56 +0900

    small-fix
    ・cssの簡略化
    ・小さな修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
     end
     collection do
       get 'complete', to: 'items#complete'
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
+      get 'category_children', defaults: { format: 'json' }
+      get 'category_grandchildren', defaults: { format: 'json' }
     end
   end
 
