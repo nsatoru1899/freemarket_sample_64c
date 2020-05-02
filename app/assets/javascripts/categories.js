4b595ebaf234c756ab25e3dd45f71cc1958a01a6
taruyusung <58249888+taruyusung@users.noreply.github.com>
2020-05-02 16:42:35 +0900

    categories.js turbolinks

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).on('turbolinks:load', ()=> { 
 
     function appendOption(category) {
       var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
